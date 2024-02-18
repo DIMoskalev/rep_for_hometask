93ad4401cfc6d4dfaf4129c423eaf85472b621c1
Dmitry Moskalev <moskalev199618120@gmail.com>
2024-02-18 23:09:29 +0300

    fix bags and upgrade cofr

--- str_func.py
+++ str_func.py
@@ -1,6 +1,4 @@
 def capital_letters(input_str):
     """Функция принимает на вход строку и возвращает ее со всеми заглавными буквами"""
-    a = input_str.upper()
-    return a
-
-print(capital_letters("Привет, как дела?"))
+    in_str = input_str.upper()
+    return in_str

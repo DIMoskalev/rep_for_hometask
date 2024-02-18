b04b84669e7412e6809f20d1fce25382de7e25ab
Dmitry Moskalev <moskalev199618120@gmail.com>
2024-02-18 22:47:33 +0300

    update file str_func.py with def first_capital_letters

--- str_func.py
+++ str_func.py
@@ -2,4 +2,11 @@ def capital_letters(input_str):
     a = input_str.upper()
     return a
 
-print(capital_letters("Привет, как дела?"))
+
+def first_capital_letters(input_str):
+    """
+    Функция делает заглавными первые буквы каждого слова в строке, поступившей на вход функции
+    """
+    a = input_str.title()
+    return a
+
